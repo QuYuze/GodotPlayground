f729762dae2db4a37b0e06af41a85ffabfdade15
Qu Yuze <quyuze09@outlook.com>
2026-03-11 10:31:50 +0800

    3/11/2026

--- dodge-the-creep/Main.cs
+++ dodge-the-creep/Main.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.ComponentModel;
 
 public partial class Main : Node
 {
@@ -37,8 +38,37 @@ public partial class Main : Node
 		GetNode<Timer>("ScoreTimer").Start();
 	}
 
+	private void OnMobTimerTimeout()
+	{
+		//create new instance of Mob Scene
+		Mob mob = MobScene.Instantiate<Mob>();
+
+		//choose random location on Path2D
+		var mobSpawnLocation = GetNode<PathFollow2D>("MobPath/MobSpawnLocation");
+		mobSpawnLocation.ProgressRatio = GD.Randf();
+
+		//set mob's direction perpendicular to the path direction
+		float direction = mobSpawnLocation.Rotation + Mathf.Pi / 2;
+
+		//add some randomness to the directions
+		direction += (float) GD.RandRange(-Mathf.Pi / 4, Math.PI / 4);
+		mob.Rotation = direction;
+
+		//set the mob position to a random location
+		mob.Position = mobSpawnLocation.Position;
+
+		//choose velocity
+		var velocity = new Vector2((float) GD.RandRange(150.0, 250.0), 0);
+		mob.LinearVelocity = velocity.Rotated(direction);
+
+		//spawn the mob by add it to the main scene
+		AddChild(mob);
+
+	}
+
 	public override void _Ready()
 	{
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
